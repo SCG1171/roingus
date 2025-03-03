6b862c38ebef085e1e656a51beccc5a5fcc8bd49
SCG1171 <144190458+SCG1171@users.noreply.github.com>
2025-03-02 21:18:09 -0800

    Update ping.ts

--- server/bot/slashCommands/ping.ts
+++ server/bot/slashCommands/ping.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 
 export const command = new SlashCommandBuilder()
   .setName('ping')
-  .setDescription('Check if the bot is responsive');
+  .setDescription('Check roingus\'s ping and responsiveness.');
 
 export async function execute(interaction: ChatInputCommandInteraction) {
   const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
