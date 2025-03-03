9e30889a4977bac1b0d5000cb301ea0c6ce984fd
SCG1171 <144190458+SCG1171@users.noreply.github.com>
2025-03-02 21:17:42 -0800

    Update ping.ts

--- server/bot/slashCommands/ping.ts
+++ server/bot/slashCommands/ping.ts
@@ -7,5 +7,5 @@ export const command = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction) {
   const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
   const latency = sent.createdTimestamp - interaction.createdTimestamp;
-  await interaction.editReply(`Pong! Latency is ${latency}ms.`);
+  await interaction.editReply(`${latency}ms`);
 }
